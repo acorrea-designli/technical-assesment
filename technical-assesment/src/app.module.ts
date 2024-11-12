585f752af41d61148bbfbbce9b9b17c00f0738ff
Andres <andres.correa@designli.co>
2024-11-12 16:16:56 -0500

    chore: clean code

--- technical-assesment/src/app.module.ts
+++ technical-assesment/src/app.module.ts
@@ -1,5 +1,4 @@
 import { Module } from '@nestjs/common'
-import { AppService } from './app.service'
 import { ModulesModule } from './modules/modules.module'
 import { CommonsModule } from './commons/commons.module'
 import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler'
